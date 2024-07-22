08273f27958a115ddd1bf6484ed53b22dc6762e6
nuhmansha <155939879+nuhmansha@users.noreply.github.com>
2024-07-22 20:07:15 +0530

    Update axiosInstance.js

--- src/Instance/axiosInstance.js
+++ src/Instance/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://fsd-challenge-backend.onrender.com",
+  baseURL: "http://localhost:7000",
   withCredentials: true,
 });
 
