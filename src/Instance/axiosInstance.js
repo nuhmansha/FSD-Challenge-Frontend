b56930202b469e3e8ae4aa98efdd71fd759d753c
nuhmansha <nuhmanibnunazar@gmail.com>
2024-07-22 20:41:48 +0530

    path updated

--- src/Instance/axiosInstance.js
+++ src/Instance/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:7000",
+  baseURL: "https://fsd-challenge-backend.onrender.com",
   withCredentials: true,
 });
 
