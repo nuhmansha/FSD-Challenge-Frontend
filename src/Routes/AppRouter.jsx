8d730e886846839b0d51e21b0ca5a327eeabb58e
nuhmansha <nuhmanibnunazar@gmail.com>
2024-07-22 13:23:46 +0530

    Add routing and protected routes to React application

--- src/Routes/AppRouter.jsx
+++ src/Routes/AppRouter.jsx
@@ -1,28 +1,36 @@
+import React from "react";
 
-
-import React from 'react'
-
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from '../Components/Navbar';
-import SignupPage from '../Pages/SignupPage';
-import LoginPage from '../Pages/LoginPage';
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import Navbar from '../Components/Navbar';
+import SignupPage from "../Pages/SignupPage";
+import LoginPage from "../Pages/LoginPage";
+import HomePage from "../Pages/HomePage";
+import ProtectedRoute from "./ProtectedRoute";
+import TaskDetailsPage from "../Components/Task/TaskDetailPage";
+import { AuthProvider } from "../Context/AuthContext";
 const AppRouter = () => {
   return (
-    <Router>
-    <Navbar />
-    <Routes>
-         
-      {/* <Route path="/" element={<Navbar />} /> */}
+    <AuthProvider>
+      <Router>
+        {/* <Navbar /> Include Navbar here to show it on every page */}
+        <Routes>
+          <Route path="/" element={<HomePage />} />{" "}
+          {/* HomePage handles its own protection */}
+          <Route
+            path="/tasks/:taskId"
+            element={
+              <ProtectedRoute>
+                <TaskDetailsPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          {/* Add other routes here as needed */}
+        </Routes>
+      </Router>
+    </AuthProvider>
+  );
+};
 
-
-      {/* <Route path="/" element={<HomePage />} /> */}
-      {/* <Route path="/tasks" element={<TasksPage />} /> */}
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignupPage/>} />
-    </Routes>
-  </Router>
-  )
-}
-
-export default AppRouter
+export default AppRouter;
--- src/Routes/ProtectedRoute.jsx
+++ src/Routes/ProtectedRoute.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import { Navigate } from 'react-router-dom'
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../Context/AuthContext"; // Import useAuth
 
-const ProtectedRoute = ({children}) => {
-    const token = localStorage.getItem("token");
-    return token ? children : <Navigate to="/login" />;
-}
+const ProtectedRoute = ({ children }) => {
+  const { user } = useAuth(); // Get user from context
 
-export default ProtectedRoute
+  return user ? children : <Navigate to="/login" />;
+};
+
+export default ProtectedRoute;
